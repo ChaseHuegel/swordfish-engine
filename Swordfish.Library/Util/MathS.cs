605e76e00c72b426916ce35ebc219142ee251968
Seven <chase@chasehuegel.io>
2022-07-12 00:38:43 -0400

    Cancellable EventHandler, MathS.Clamp

--- Swordfish.Library/Util/MathS.cs
+++ Swordfish.Library/Util/MathS.cs
@@ -51,5 +51,60 @@ namespace Swordfish.Library.Util
         {
             return _x + (_width * _y);
         }
+
+        public static sbyte Clamp(sbyte value, sbyte min, sbyte max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static byte Clamp(byte value, byte min, byte max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static short Clamp(short value, short min, short max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static ushort Clamp(ushort value, ushort min, ushort max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static int Clamp(int value, int min, int max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static uint Clamp(uint value, uint min, uint max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static long Clamp(long value, long min, long max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static ulong Clamp(ulong value, ulong min, ulong max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static double Clamp(double value, double min, double max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        public static decimal Clamp(decimal value, decimal min, decimal max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
     }
 }
