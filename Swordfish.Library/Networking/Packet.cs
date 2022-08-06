5c019a7862cef406f8858f6cdcd5b3a967aa1c64
Seven <chase@chasehuegel.io>
2022-08-06 19:23:19 -0400

    add ordering to Packet fields

--- Swordfish.Library/Networking/Packet.cs
+++ Swordfish.Library/Networking/Packet.cs
@@ -5,8 +5,13 @@ namespace Swordfish.Library.Networking
 {
     public class Packet : IDataBody
     {
+        [DataField(0)]
         public int SessionID;
+        
+        [DataField(1)]
         public int PacketID;
+
+        [DataField(2)]
         public uint Sequence;
     }
 }
