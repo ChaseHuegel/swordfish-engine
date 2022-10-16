dde1fff2d4e0ec4f3785662aaa89bf2483b880ce
Seven <chase@chasehuegel.io>
2022-10-16 16:33:40 -0400

    add BehaviorAction

---
+++ Swordfish.Library/BehaviorTrees/BehaviorAction.cs
@@ -0,0 +1,13 @@
+namespace Swordfish.Library.BehaviorTrees
+{
+    public abstract class BehaviorAction<TTarget> : BehaviorNode<TTarget> where TTarget : class
+    {
+        public override BehaviorState Evaluate(TTarget target, float delta)
+        {
+            Run(target, delta);
+            return BehaviorState.SUCCESS;
+        }
+
+        public abstract void Run(TTarget target, float delta);
+    }
+}
