f6d1cb0d4c71ee06d2049ae8c68ea0940b178139
Seven <chase@chasehuegel.io>
2022-10-22 12:47:47 -0400

    added Property and PropertyCollection

---
+++ Swordfish.Library/Collections/Property.cs
@@ -0,0 +1,108 @@
+﻿using Swordfish.Library.Types;
+using Swordfish.Library.Util;
+
+namespace Swordfish.Library.Collections
+{
+    public class Property
+    {
+        public string Name
+        {
+            get => NameBinding.Get();
+            set => NameBinding.Set(value);
+        }
+
+        public float Value
+        {
+            get => ValueBinding.Get();
+            set
+            {
+                float newValue = MathS.Clamp(value, 0f, MaxValue);
+                ValueBinding.Set(newValue);
+            }
+        }
+
+        public float MaxValue
+        {
+            get => MaxValueBinding.Get();
+            set
+            {
+                float oldMax = MaxValueBinding.Get();
+                float newMax = value > 0f ? value : float.MaxValue;
+
+                if (oldMax > 0f)
+                    Value *= newMax / oldMax;
+
+                MaxValueBinding.Set(newMax);
+            }
+        }
+
+        public DataBinding<string> NameBinding { get; private set; }
+        public DataBinding<float> ValueBinding { get; private set; }
+        public DataBinding<float> MaxValueBinding { get; private set; }
+
+        public Property(string name, float value = 1.0f, float max = 0.0f)
+        {
+            NameBinding = new DataBinding<string>();
+            ValueBinding = new DataBinding<float>();
+            MaxValueBinding = new DataBinding<float>();
+
+            Name = name;
+            Value = value;
+            MaxValue = max;
+        }
+
+        public bool IsMax() => Value == MaxValue;
+        public float CalculatePercent() => Value / MaxValue;
+
+        public Property Add(float amount)
+        {
+            Value += amount;
+            return this;
+        }
+
+        public Property Remove(float amount)
+        {
+            Value -= amount;
+            return this;
+        }
+
+        public Property Maximize()
+        {
+            Value = MaxValue;
+            return this;
+        }
+
+        public Property Clear()
+        {
+            Value = 0;
+            return this;
+        }
+
+        public float PeekAdd(float amount)
+        {
+            return MathS.Clamp(Value + amount, 0, MaxValue);
+        }
+
+        public float PeekRemove(float amount)
+        {
+            return MathS.Clamp(Value - amount, 0, MaxValue);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is string str)
+                return Name.Equals(str);
+
+            if (obj is Property atr)
+                return Name.Equals(atr.Name);
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+}
---
+++ Swordfish.Library/Collections/PropertyCollection.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace Swordfish.Library.Collections
+{
+    public class PropertyCollection
+    {
+        private readonly Dictionary<string, Property> Items = new Dictionary<string, Property>();
+
+        public bool Contains(string name)
+        {
+            return Items.ContainsKey(name);
+        }
+
+        public float ValueOf(string name)
+        {
+            return Items.TryGetValue(name, out Property attribute) ? attribute.Value : 0f;
+        }
+
+        public float MaxValueOf(string name)
+        {
+            return Items.TryGetValue(name, out Property attribute) ? attribute.MaxValue : 0f;
+        }
+
+        public float CalculatePercentOf(string name)
+        {
+            return Items.TryGetValue(name, out Property attribute) ? attribute.CalculatePercent() : 0f;
+        }
+
+        public Property Get(string name)
+        {
+            return Items.TryGetValue(name, out Property attribute) ? attribute : null;
+        }
+
+        public Property Add(string name, float value, float max = 0f)
+        {
+            if (!Items.ContainsKey(name))
+            {
+                var attribute = new Property(name, value, max);
+                Items.Add(name, attribute);
+                return attribute;
+            }
+
+            return null;
+        }
+
+        public bool TryAdd(string name, float value, float max = 0f)
+        {
+            if (Items.ContainsKey(name))
+                return false;
+            else
+            {
+                Items.Add(name, new Property(name, value, max));
+                return true;
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return Items.Remove(name);
+        }
+
+        public Property GetOrAdd(string name, float value, float max = 0f)
+        {
+            return Get(name) ?? Add(name, value, max);
+        }
+    }
+
+}
