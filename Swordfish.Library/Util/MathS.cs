c85a3001b92ba2d0a783be262402a5705150bed5
Seven <chase@chasehuegel.io>
2024-10-06 02:16:22 -0400

    add Repeat and PingPong to math

--- Swordfish.Library/Util/MathS.cs
+++ Swordfish.Library/Util/MathS.cs
@@ -13,31 +13,36 @@ namespace Swordfish.Library.Util
         private static readonly MemorizedSin FastSin = MemorizedSin.ConstructByMaxError(0.001f);
         private static readonly MemorizedCos FastCos = MemorizedCos.ConstructByMaxError(0.001f);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Sin(float value)
         {
             return FastSin.CalculateUnbound(value);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Cos(float value)
         {
             return FastCos.CalculateUnbound(value);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Lerp(float start, float end, float value)
         {
-            return start + value * (end - start);
+            return start * (1 - value) + end * value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double PingPong(double time, double max)
+        {
+            return max - Math.Abs(Repeat(time, max * 2) - max);
+        }
+
+        public static double Repeat(double time, double max)
+        {
+            return Math.Clamp(time - Math.Floor(time / max) * max, 0, max);
+        }
+
         public static float Slerp(float start, float end, float value)
         {
             return (float)Math.Pow(end * Math.Pow(start, -1), value) * start;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetOverflow(int value, int min, int max)
         {
             if (value < min) return value - min;
@@ -46,7 +51,6 @@ namespace Swordfish.Library.Util
             return 0;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float GetOverflow(float value, float min, float max)
         {
             if (value < min) return value - min;
@@ -55,7 +59,6 @@ namespace Swordfish.Library.Util
             return 0.0f;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int WrapInt(int _value, int _rangeMin, int _rangeMax)
         {
             int range = _rangeMax - _rangeMin + 1;
@@ -68,79 +71,66 @@ namespace Swordfish.Library.Util
             return _rangeMin + (_value - _rangeMin) % range;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float RangeToRange(float _input, float _low, float _high, float _newLow, float _newHigh)
         {
             return ((_input - _low) / (_high - _low)) * (_newHigh - _newLow) + _newLow;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int IndexFrom2D(int _x, int _y, int _width)
         {
             return _x + (_width * _y);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static sbyte Clamp(sbyte value, sbyte min, sbyte max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static byte Clamp(byte value, byte min, byte max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static short Clamp(short value, short min, short max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ushort Clamp(ushort value, ushort min, ushort max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Clamp(int value, int min, int max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static uint Clamp(uint value, uint min, uint max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static long Clamp(long value, long min, long max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ulong Clamp(ulong value, ulong min, ulong max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Clamp(float value, float min, float max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Clamp(double value, double min, double max)
         {
             return value < min ? min : value > max ? max : value;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static decimal Clamp(decimal value, decimal min, decimal max)
         {
             return value < min ? min : value > max ? max : value;
