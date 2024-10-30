1d70b60944400f3731de93645a94f57e66149f00
Seven <chase@chasehuegel.io>
2024-10-29 21:32:21 -0400

    rearrange exit codes

--- Swordfish/ExitCode.cs
+++ Swordfish/ExitCode.cs
@@ -8,12 +8,12 @@ public enum ExitCode
     Normal = 0,
     
     /// <summary>
-    ///     Reached an unexpected state. This is a fatal error.
-    /// </summary>
-    BadState = 1,
-    
-    /// <summary>
     ///     An unexpected crash occurred.
     /// </summary>
-    Crash = 2,
+    Crash = 1,
+    
+    /// <summary>
+    ///     Reached an unexpected state. This is a fatal error.
+    /// </summary>
+    BadState = 2,
 }
