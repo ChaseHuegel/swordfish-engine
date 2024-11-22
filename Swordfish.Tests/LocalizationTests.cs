703502f32b34e22beac52cb4f354faa5c6b3fd55
Seven <chase@chasehuegel.io>
2024-11-22 00:36:59 -0500

    add localization tests

---
+++ Swordfish.Tests/LocalizationTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Shoal.Globalization;
+using SmartFormat;
+using SmartFormat.Extensions;
+using Swordfish.Library.Globalization;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Swordfish.Tests;
+
+// ReSharper disable once UnusedType.Global
+public class LocalizationTests(ITestOutputHelper output) : TestBase(output)
+{
+    protected override void OnSetup()
+    {
+        var enLang = new Language("en");
+        enLang.Translations.Add("Test.KVP.Pair", "{Key}={Value}");
+        enLang.Translations.Add("Test.KVP.Key", "Key");
+        enLang.Translations.Add("Test.KVP.Value", "Value");
+
+        Language[] languages =
+        [
+            enLang,
+        ];
+
+        Smart.Default.Settings.Localization.LocalizationProvider = new Localization(languages);
+        Smart.Default.AddExtensions(new LocalizationFormatter());
+    }
+
+    protected override void OnTearDown()
+    {
+        Smart.Default.Settings.Localization.LocalizationProvider = null;
+    }
+
+    [Fact]
+    public void CanFormatLanguages()
+    {
+        var kvp = new KeyValuePair<string ,string>("hello", "world");
+
+        string formattedKVP = Smart.Format("{:L:Test.KVP.Pair}", kvp);
+        string formattedKey = Smart.Format("{:L:Test.KVP.Key}", kvp);
+        string formattedValue = Smart.Format("{:L:Test.KVP.Value}", kvp);
+        
+        Assert.Equal($"{kvp.Key}={kvp.Value}", formattedKVP);
+        Assert.Equal("Key", formattedKey);
+        Assert.Equal("Value", formattedValue);
+    }
+}
--- Swordfish.Tests/TestBase.cs
+++ Swordfish.Tests/TestBase.cs
@@ -1,13 +1,37 @@
+using System;
+using DryIoc;
 using Xunit.Abstractions;
 
 namespace Swordfish.Tests;
 
-public class TestBase
+public abstract class TestBase : IDisposable
 {
     protected readonly ITestOutputHelper Output;
+    
+    private readonly Container _container;
 
     public TestBase(ITestOutputHelper output)
     {
         Output = output;
+        _container = new Container();
+        
+        Setup();
     }
+    
+    private void Setup()
+    {
+        SetupContainer(_container);
+        _container.ValidateAndThrow();
+        OnSetup();
+    }
+
+    void IDisposable.Dispose()
+    {
+        OnTearDown();
+        _container.Dispose();
+    }
+
+    protected virtual void SetupContainer(Container container) {}
+    protected virtual void OnSetup() {}
+    protected virtual void OnTearDown() {}
 }
