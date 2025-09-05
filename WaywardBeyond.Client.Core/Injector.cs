5273a90bacd013cf782e9611aa6a524d4dfd536b
Seven <chase@chasehuegel.io>
2025-09-04 20:04:15 -0400

    register parsers and cleanup Injector

--- WaywardBeyond.Client.Core/Injector.cs
+++ WaywardBeyond.Client.Core/Injector.cs
@@ -5,7 +5,9 @@ using Swordfish.ECS;
 using Swordfish.Graphics;
 using Swordfish.IO;
 using Swordfish.Library.IO;
+using Swordfish.Library.Serialization.Toml;
 using WaywardBeyond.Client.Core.Bricks;
+using WaywardBeyond.Client.Core.Items;
 using WaywardBeyond.Client.Core.Systems;
 using WaywardBeyond.Client.Core.UI;
 
@@ -17,19 +19,41 @@ public class Injector : IDryIocInjector
     public void Inject(IContainer container)
     {
         container.RegisterMany<Entry>();
-        container.Register<IEntryPoint, PlayerInteractionSystem>();
+
+        RegisterUI(container);
+        RegisterInput(container);
+        RegisterTomlParsers(container);
+        RegisterEntitySystems(container);
         
-        container.Register<Hotbar>(Reuse.Singleton);
-        container.RegisterMapping<IEntitySystem, Hotbar>();
-        
-        container.Register<IEntitySystem, PlayerControllerSystem>();
-        container.Register<IEntitySystem, FirstPersonCameraSystem>();
-        container.Register<IEntitySystem, CleanupMeshRendererSystem>();
-        container.Register<IEntitySystem, ThrusterSystem>();
-        
+        //  TODO this was thrown together for testing and needs cleaned up
         container.Register<BrickEntityBuilder>(Reuse.Singleton);
         container.RegisterDelegate<Shader>(context => context.Resolve<IFileParseService>().Parse<Shader>(AssetPaths.Shaders.At("lightedArray.glsl")), Reuse.Singleton);
         container.RegisterDelegate<TextureArray>(context => context.Resolve<IFileParseService>().Parse<TextureArray>(AssetPaths.Textures.At("block\\")), Reuse.Singleton);
         container.RegisterDelegate<DataStore>(context => context.Resolve<IECSContext>().World.DataStore, Reuse.Singleton);
+    }
+
+    private static void RegisterUI(IContainer container)
+    {
+        container.Register<Hotbar>(Reuse.Singleton);
+        container.RegisterMapping<IEntitySystem, Hotbar>();
+    }
+    
+    private static void RegisterInput(IContainer container)
+    {
+        container.Register<IEntryPoint, PlayerInteractionSystem>();
+    }
+    
+    private static void RegisterTomlParsers(IContainer container)
+    {
+        container.RegisterMany<TomlParser<BrickDefinitions>>(Reuse.Singleton, ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
+        container.RegisterMany<TomlParser<ItemDefinitions>>(Reuse.Singleton, ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
+    }
+
+    private static void RegisterEntitySystems(IContainer container)
+    {
+        container.Register<IEntitySystem, PlayerControllerSystem>();
+        container.Register<IEntitySystem, FirstPersonCameraSystem>();
+        container.Register<IEntitySystem, CleanupMeshRendererSystem>();
+        container.Register<IEntitySystem, ThrusterSystem>();
     }
 }
