765785b401458df0638d4c1a8c4e77fb314b8443
Seven <chase@chasehuegel.io>
2025-09-05 21:17:20 -0400

    Extend Result objects to support exceptions #138

--- Swordfish.Library/Util/Result.cs
+++ Swordfish.Library/Util/Result.cs
@@ -3,31 +3,40 @@
 // ReSharper disable MemberCanBePrivate.Global
 
 #nullable enable
+using System;
+
 namespace Swordfish.Library.Util;
 
-public readonly struct Result(in bool success, in string? message = null)
+public readonly struct Result(in bool success, in string? message = null, in Exception? exception = null)
 {
     public readonly bool Success = success;
     public readonly string? Message = message;
-    //  TODO add an exception
+    public readonly Exception? Exception = exception;
 
     public static implicit operator bool(Result result) => result.Success;
+    public static implicit operator Exception(Result result) => new(result.Message);
     
     public static Result FromSuccess(string? message = null) => new(true, message);
 
     public static Result FromFailure(string? message = null) => new(false, message);
+    
+    public static Result FromFailure(Exception? exception = null) => new(false, exception?.Message, exception);
 }
 
-public readonly struct Result<T>(in bool success, in T value, in string? message = null)
+public readonly struct Result<T>(in bool success, in T value, in string? message = null, in Exception? exception = null)
 {
     public readonly bool Success = success;
     public readonly string? Message = message;
     public readonly T Value = value;
+    public readonly Exception? Exception = exception;
 
     public static implicit operator bool(Result<T> result) => result.Success;
     public static implicit operator T(Result<T> result) => result.Value;
+    public static implicit operator Exception(Result<T> result) => new(result.Message);
 
     public static Result<T> FromSuccess(T value, string? message = null) => new(true, value, message);
 
     public static Result<T> FromFailure(string? message = null) => new(false, default!, message);
+    
+    public static Result<T> FromFailure(Exception? exception = null) => new(false, default!, exception?.Message, exception);
 }
