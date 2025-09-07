d4af4b7dbab5e4e8e5b5649afb59dc27985ce1c2
Seven <chase@chasehuegel.io>
2025-09-06 20:36:10 -0400

    Introduce a mesh IAssetDatabase #146

---
+++ Swordfish/IO/MeshDatabase.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Logging;
+using Shoal.DependencyInjection;
+using Shoal.Modularity;
+using Swordfish.Graphics;
+using Swordfish.Library.IO;
+
+namespace Swordfish.IO;
+
+/// <summary>
+///     Provides access to <see cref="Mesh"/>s from virtual resources.
+/// </summary>
+internal sealed class MeshDatabase : SimpleVirtualAssetDatabase<Mesh>, IAutoActivate
+{
+    public MeshDatabase(
+        in ILogger<MeshDatabase> logger,
+        in IFileParseService fileParseService,
+        in VirtualFileSystem vfs)
+        : base(logger, fileParseService, vfs)
+    {
+        Load();
+    }
+    
+    /// <inheritdoc/>
+    protected override bool IsValidFile(PathInfo path) => path.HasExtension(".obj");
+
+    /// <inheritdoc/>
+    protected override PathInfo GetRootPath() => AssetPaths.Models;
+}
