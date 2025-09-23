5fd3e5e33429c8b672d673b7373d622f2b5e2224
Seven <chase@chasehuegel.io>
2025-09-23 00:49:02 -0400

    Child entities should be able to have local transform offsets #163 (#164)
    * Child entities should be able to have local transform offsets #163
    
    * Version up Swordfish

--- Swordfish/ECS/ChildComponent.cs
+++ Swordfish/ECS/ChildComponent.cs
@@ -1,6 +1,11 @@
+using System.Numerics;
+
 namespace Swordfish.ECS;
 
 public struct ChildComponent(in int parent) : IDataComponent
 {
     public int Parent = parent;
+    public Vector3 LocalPosition;
+    public Quaternion LocalOrientation;
+    public Vector3 LocalScale = Vector3.One;
 }
