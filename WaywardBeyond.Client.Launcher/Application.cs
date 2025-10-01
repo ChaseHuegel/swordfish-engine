998473a6f287fc6e5c15201aeff7e273859bc620
Seven <chase@chasehuegel.io>
2025-09-30 23:33:26 -0400

    Move wayward beyond dependencies into a lib folder #211

---
+++ WaywardBeyond.Client.Launcher/Application.cs
@@ -0,0 +1,13 @@
+﻿using Swordfish;
+
+namespace WaywardBeyond.Client.Launcher;
+
+public class Application(in string[] args)
+{
+    private readonly SwordfishEngine _engine = new(args);
+
+    public int Run()
+    {
+        return _engine.Run();
+    }
+}
--- WaywardBeyond.Client.Launcher/Program.cs
+++ WaywardBeyond.Client.Launcher/Program.cs
@@ -1,15 +1,69 @@
-﻿using Swordfish;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
 
 namespace WaywardBeyond.Client.Launcher;
 
 internal static class Program
 {
+    private static readonly string _libDir;
+    
+    static Program()
+    {
+        string baseDir = AppContext.BaseDirectory;
+        _libDir = Path.Combine(baseDir, "lib");
+    }
+    
     private static int Main(string[] args)
     {
-        var engine = new SwordfishEngine(args);
+        AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolver;
+        NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), DllImportResolver);
 
-        int exitCode = engine.Run();
+        var application = new Application(args);
+        return application.Run();
+    }
 
-        return exitCode;
+    private static Assembly? AssemblyResolver(object? sender, ResolveEventArgs eventArgs)
+    {
+        try
+        {
+            string? name = new AssemblyName(eventArgs.Name).Name;
+            string candidate = Path.Combine(_libDir, name + ".dll");
+            if (File.Exists(candidate))
+            {
+                return AssemblyLoadContext.Default.LoadFromAssemblyPath(candidate);
+            }
+        }
+        catch
+        {
+            // Allow other resolvers to attempt
+        }
+
+        return null;
+    }
+
+    private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        string fileName = libraryName;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            fileName += ".dll";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            fileName = "lib" + libraryName + ".so";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            fileName = "lib" + libraryName + ".dylib";
+        }
+
+        string candidate = Path.Combine(_libDir, fileName);
+        if (!File.Exists(candidate))
+        {
+            return IntPtr.Zero;
+        }
+
+        return NativeLibrary.TryLoad(candidate, out IntPtr handle) ? handle : IntPtr.Zero;
     }
 }
