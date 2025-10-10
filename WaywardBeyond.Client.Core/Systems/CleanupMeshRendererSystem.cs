1d4a59c62f0c50083a6129dcbc79efe49281fe2b
Seven <chase@chasehuegel.io>
2025-10-10 00:25:31 -0400

    Brick meshes flicker when being modified #204

--- WaywardBeyond.Client.Core/Systems/CleanupMeshRendererSystem.cs
+++ WaywardBeyond.Client.Core/Systems/CleanupMeshRendererSystem.cs
@@ -1,12 +1,18 @@
 using Swordfish.ECS;
+using Swordfish.Graphics;
 using WaywardBeyond.Client.Core.Components;
 
 namespace WaywardBeyond.Client.Core.Systems;
 
-internal sealed class CleanupMeshRendererSystem : EntitySystem<MeshRendererCleanup>
+internal sealed class CleanupMeshRendererSystem : EntitySystem<MeshRendererCleanup, MeshRendererComponent>
 {
-    protected override void OnTick(float delta, DataStore store, int entity, ref MeshRendererCleanup meshRendererCleanup)
+    protected override void OnTick(float delta, DataStore store, int entity, ref MeshRendererCleanup meshRendererCleanup, ref MeshRendererComponent meshRendererComponent)
     {
+        if (!meshRendererComponent.Bound)
+        {
+            return;
+        }
+        
         meshRendererCleanup.MeshRenderer.Dispose();
         meshRendererCleanup.MeshRenderer.Mesh.Dispose();
         store.Remove<MeshRendererCleanup>(entity);
