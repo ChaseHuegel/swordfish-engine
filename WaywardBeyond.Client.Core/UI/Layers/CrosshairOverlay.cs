268117e5afd3c366942417bc0ad7dce5aaa8f63d
Seven <chase@chasehuegel.io>
2025-10-10 00:05:12 -0400

    Update crosshair

--- WaywardBeyond.Client.Core/UI/Layers/CrosshairOverlay.cs
+++ WaywardBeyond.Client.Core/UI/Layers/CrosshairOverlay.cs
@@ -43,8 +43,8 @@ internal class CrosshairOverlay : IUILayer
                 Anchors = Anchors.Center,
                 X = new Relative(0.5f),
                 Y = new Relative(0.5f),
-                Width = new Fixed(64),
-                Height = new Fixed(64),
+                Width = new Fixed((int)(ui.Width * 0.01f)),
+                Height = new Fixed((int)(ui.Width * 0.01f)),
             };
         }
         
