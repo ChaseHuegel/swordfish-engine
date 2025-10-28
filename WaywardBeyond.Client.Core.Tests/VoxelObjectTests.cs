0f5a15f80853e23c6b04d1bf9e64b4e08f008e05
Seven <chase@chasehuegel.io>
2025-10-27 21:41:52 -0400

    wip tests proj

---
+++ WaywardBeyond.Client.Core.Tests/VoxelObjectTests.cs
@@ -0,0 +1,53 @@
+using WaywardBeyond.Client.Core.Voxels;
+
+namespace WaywardBeyond.Client.Core.Tests;
+
+public class VoxelObjectTests
+{
+    [Test]
+    public void Test1()
+    {
+        var voxelObject = new VoxelObject(chunkSize: 16);
+        
+        voxelObject.Set(0, 0, 0, new Voxel(1, 0, 0));
+        voxelObject.Set(8, 8, 8, new Voxel(1, 0, 0));
+        voxelObject.Set(15, 15, 15, new Voxel(1, 0, 0));
+        voxelObject.Set(16, 16, 16, new Voxel(1, 0, 0));
+        voxelObject.Set(24, 24, 24, new Voxel(1, 0, 0));
+        voxelObject.Set(31, 31, 31, new Voxel(1, 0, 0));
+
+        foreach (Voxel voxel in voxelObject)
+        {
+            if (voxel.ID == 0)
+            {
+                continue;
+            }
+            Console.WriteLine("Found voxel: " + voxel.ID);
+        }
+        
+        foreach (ref Voxel voxel in voxelObject)
+        {
+            if (voxel.ID == 0)
+            {
+                continue;
+            }
+            voxel.ID = 2;
+        }
+        
+        Console.WriteLine();
+        
+        foreach (Voxel voxel in voxelObject)
+        {
+            if (voxel.ID == 0)
+            {
+                continue;
+            }
+            Console.WriteLine("Found voxel: " + voxel.ID);
+        }
+        
+        Console.WriteLine();
+
+        VoxelSample sample = voxelObject.Sample(0, 0, 0);
+        Console.WriteLine($"Sample: {sample.Center.ID}, Above: {sample.Above.ID}, Below: {sample.Below.ID}, Left: {sample.Left.ID}, Right: {sample.Right.ID}, Ahead: {sample.Ahead.ID}, Behind: {sample.Behind.ID}");
+    }
+}
