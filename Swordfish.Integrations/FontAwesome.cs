a45a9a01ec0842bf6aea35bfa5ee126400f67eb9
Seven <chase@chasehuegel.io>
2025-12-18 01:47:06 -0500

    Add additional FontAwesome unicodes

--- Swordfish.Integrations/FontAwesome.cs
+++ Swordfish.Integrations/FontAwesome.cs
@@ -1,14 +1,15 @@
 // ReSharper disable UnusedMember.Global
-namespace Swordfish.Integrations
+namespace Swordfish.Integrations;
+
+public static class FontAwesome
 {
-    public static class FontAwesome
-    {
-        public const string KEYBOARD = "\uf11c";
-        public const string BUG = "\uf188";
-        public const string CHART_BAR = "\uf080";
-        public const string DIAGRAM_PROJECT = "\uf542";
-        public const string TERMINAL = "\uf120";
-        public const string CIRCLE_QUESTION = "\uf059";
-        public const string MINUS = "\uf068";
-    }
+    public const string KEYBOARD = "\uf11c";
+    public const string BUG = "\uf188";
+    public const string CHART_BAR = "\uf080";
+    public const string DIAGRAM_PROJECT = "\uf542";
+    public const string TERMINAL = "\uf120";
+    public const string CIRCLE_QUESTION = "\uf059";
+    public const string MINUS = "\uf068";
+    public const string CIRCLE = "\uf111";
+    public const string CIRCLE_DOT = "\uf192";
 }
