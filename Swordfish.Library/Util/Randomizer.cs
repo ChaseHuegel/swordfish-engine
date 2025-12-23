6d1d0059ab59f198e05fd17446fafe2ee66d6d16
Seven <chase@chasehuegel.io>
2025-12-23 04:52:10 -0500

    Add additional overloads to Randomizer

--- Swordfish.Library/Util/Randomizer.cs
+++ Swordfish.Library/Util/Randomizer.cs
@@ -18,39 +18,45 @@ public class Randomizer
     {
         _random = new Random(seed);
     }
-
+    
     public float NextFloat()
     {
         return (float)_random.NextDouble();
     }
-
+    
     public double NextDouble()
     {
         return _random.NextDouble();
     }
-
+    
     public int NextInt()
     {
         return _random.Next();
     }
-
+    
     public int NextInt(int maxValue)
     {
         return _random.Next(maxValue);
     }
-
+    
     public int NextInt(int minValue, int maxValue)
     {
         return _random.Next(minValue, maxValue);
     }
-
+    
     public T Select<T>(params T[] items)
     {
         int index = _random.Next(items.Length);
         return items[index];
     }
-
-    public T Select<T>(List<T> list)
+    
+    public T Select<T>(IReadOnlyList<T> list)
+    {
+        int index = _random.Next(list.Count);
+        return list[index];
+    }
+    
+    public T Select<T>(IList<T> list)
     {
         int index = _random.Next(list.Count);
         return list[index];
