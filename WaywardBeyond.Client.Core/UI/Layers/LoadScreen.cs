4a7da65bb66c0262df1458a962bd35aad8ba840d
Seven <chase@chasehuegel.io>
2025-12-24 18:56:39 -0500

    Tips and lore snippets should appear on the load screen #322

--- WaywardBeyond.Client.Core/UI/Layers/LoadScreen.cs
+++ WaywardBeyond.Client.Core/UI/Layers/LoadScreen.cs
@@ -1,20 +1,37 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
 using System.Text;
 using Reef;
 using Reef.Constraints;
 using Reef.UI;
 using Swordfish.Graphics;
 using Swordfish.Integrations;
+using Swordfish.Library.Collections;
 using Swordfish.Library.Util;
+using WaywardBeyond.Client.Core.Meta;
 using WaywardBeyond.Client.Core.Saves;
 
 namespace WaywardBeyond.Client.Core.UI.Layers;
 
-internal sealed class LoadScreen(in GameSaveService gameSaveService) : IUILayer
+internal sealed class LoadScreen(
+    in GameSaveService gameSaveService,
+    in IAssetDatabase<LocalizedTags> localizedTagDatabase
+) : IUILayer
 {
+    private const float WORDS_PER_MINUTE = 180;
+    private const float WORDS_PER_SECOND = WORDS_PER_MINUTE / 60f;
+    private const float SECONDS_PER_WORD = 1f / WORDS_PER_SECOND;
+    
     private readonly GameSaveService _gameSaveService = gameSaveService;
+    private readonly IAssetDatabase<LocalizedTags> _localizedTagDatabase = localizedTagDatabase;
+    private readonly Randomizer _randomizer = new();
     
     private double _currentTime;
+    
+    private string _hint = string.Empty;
+    private double _hintStartTime;
+    private double _hintEndTime;
     
     public bool IsVisible()
     {
@@ -24,6 +41,16 @@ internal sealed class LoadScreen(in GameSaveService gameSaveService) : IUILayer
     public Result RenderUI(double delta, UIBuilder<Material> ui)
     {
         _currentTime += delta;
+
+        if (_currentTime >= _hintEndTime)
+        {
+            _hintStartTime = _currentTime;
+            Result<LocalizedTags> localizedTags = _localizedTagDatabase.Get(CultureInfo.CurrentCulture.TwoLetterISOLanguageName);
+            IReadOnlyList<string>? tags = localizedTags.Success ? localizedTags.Value.GetValues("game_hints") : null;
+        
+            _hint = tags != null ? _randomizer.Select(tags) : string.Empty;
+            _hintEndTime = _hintStartTime + CountWords(_hint) * SECONDS_PER_WORD;
+        }
 
         using (ui.Element())
         {
@@ -76,6 +103,17 @@ internal sealed class LoadScreen(in GameSaveService gameSaveService) : IUILayer
                 };
             }
             
+            using (ui.Text(_hint))
+            {
+                ui.FontSize = 14;
+                ui.Color = new Vector4(0.5f, 0.5f, 0.5f, 1f);
+                ui.Constraints = new Constraints
+                {
+                    Anchors = Anchors.Center | Anchors.Bottom,
+                    X = new Relative(0.5f),
+                };
+            }
+            
             using (ui.Element())
             {
                 ui.Constraints = new Constraints
@@ -86,5 +124,29 @@ internal sealed class LoadScreen(in GameSaveService gameSaveService) : IUILayer
         }
         
         return Result.FromSuccess();
+    }
+    
+    private static int CountWords(in string text)
+    {
+        var wordCount = 0;
+        var inWord = false;
+
+        foreach (char c in text)
+        {
+            if (char.IsWhiteSpace(c) || c == '-' || c == '—' || c == '–')
+            {
+                inWord = false;
+                continue;
+            }
+            
+            if (!inWord)
+            {
+                wordCount++;
+            }
+            
+            inWord = true;
+        }
+
+        return wordCount;
     }
 }
