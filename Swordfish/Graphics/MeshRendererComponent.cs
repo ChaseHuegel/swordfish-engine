fda2677eb95a307a40d41ee9d8ea1fdbae053533
Seven <chase@chasehuegel.io>
2025-12-29 00:19:39 -0500

    Fix MeshRendererSystem crash when an entity is freed #340

--- Swordfish/Graphics/MeshRendererComponent.cs
+++ Swordfish/Graphics/MeshRendererComponent.cs
@@ -2,16 +2,11 @@ using Swordfish.ECS;
 
 namespace Swordfish.Graphics;
 
-public struct MeshRendererComponent : IDataComponent
+public struct MeshRendererComponent(in MeshRenderer meshRenderer) : IDataComponent
 {
     public const int DefaultIndex = 3;
 
-    public readonly MeshRenderer MeshRenderer;
+    public readonly MeshRenderer? MeshRenderer = meshRenderer;
 
     public bool Bound;
-
-    public MeshRendererComponent(MeshRenderer meshRenderer)
-    {
-        MeshRenderer = meshRenderer;
-    }
 }
