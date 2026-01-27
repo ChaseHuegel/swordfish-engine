02dd20b9548ff9c815f9081cb9661244dc30badf
Seven <chase@chasehuegel.io>
2026-01-26 22:57:52 -0500

    Reef should support non-mouse input #416

--- Reef/UIController.cs
+++ Reef/UIController.cs
@@ -14,6 +14,118 @@ public sealed class UIController
         Middle = 4,
     }
     
+    public enum Input
+    {
+        Backspace = 8,
+        Tab = 9,
+        Enter = 13,
+        Shift = 16,
+        Control = 17,
+        Alt = 18,
+        Pause = 19,
+        Capslock = 20,
+        Esc = 27,
+        Space = 32,
+        PageUp = 33,
+        PageDown = 34,
+        End = 35,
+        Home = 36,
+        LeftArrow = 37,
+        UpArrow = 38,
+        RightArrow = 39,
+        DownArrow = 40,
+        Select = 41,
+        Print = 42,
+        Execute = 43,
+        PrintScreen = 44,
+        Insert = 45,
+        Delete = 46,
+        Help = 47,
+        D0 = 48,
+        D1 = 49,
+        D2 = 50,
+        D3 = 51,
+        D4 = 52,
+        D5 = 53,
+        D6 = 54,
+        D7 = 55,
+        D8 = 56,
+        D9 = 57,
+        A = 65,
+        B = 66,
+        C = 67,
+        D = 68,
+        E = 69,
+        F = 70,
+        G = 71,
+        H = 72,
+        I = 73,
+        J = 74,
+        K = 75,
+        L = 76,
+        M = 77,
+        N = 78,
+        O = 79,
+        P = 80,
+        Q = 81,
+        R = 82,
+        S = 83,
+        T = 84,
+        U = 85,
+        V = 86,
+        W = 87,
+        X = 88,
+        Y = 89,
+        Z = 90,
+        LeftWindows = 91,
+        RightWindows = 92,
+        Application = 93,
+        Sleep = 95,
+        Numpad0 = 96,
+        Numpad1 = 97,
+        Numpad2 = 98,
+        Numpad3 = 99,
+        Numpad4 = 100,
+        Numpad5 = 101,
+        Numpad6 = 102,
+        Numpad7 = 103,
+        Numpad8 = 104,
+        Numpad9 = 105,
+        Multiply = 106,
+        Add = 107,
+        Separator = 108,
+        Subtract = 109,
+        Decimal = 110,
+        Divide = 111,
+        F1 = 112,
+        F2 = 113,
+        F3 = 114,
+        F4 = 115,
+        F5 = 116,
+        F6 = 117,
+        F7 = 118,
+        F8 = 119,
+        F9 = 120,
+        F10 = 121,
+        F11 = 122,
+        F12 = 123,
+        F13 = 124,
+        F14 = 125,
+        F15 = 126,
+        F16 = 127,
+        F17 = 128,
+        F18 = 120,
+        F19 = 130,
+        F20 = 131,
+        F21 = 132,
+        F22 = 133,
+        F23 = 134,
+        F24 = 135,
+        Numlock = 144,
+        ScrollLock = 145,
+        Slash,
+    }
+    
     private readonly struct InputState<T>(T previous, T current)
     {
         public readonly T Previous = previous;
@@ -51,6 +163,8 @@ public sealed class UIController
     private InputState<bool> _leftMouse;
     private InputState<bool> _rightMouse;
     private InputState<bool> _middleMouse;
+    private string? _lastInteractedID;
+    private readonly List<Input> _inputBuffer = [];
     private readonly Dictionary<string, InteractionState> _interactionStates = [];
     
     public void UpdateMouse(int x, int y, MouseButtons downMouseButtons)
@@ -59,6 +173,15 @@ public sealed class UIController
         _leftMouse = new InputState<bool>(previous: _leftMouse.Current, current: (downMouseButtons & MouseButtons.Left) == MouseButtons.Left);
         _rightMouse = new InputState<bool>(previous: _rightMouse.Current, current: (downMouseButtons & MouseButtons.Right) == MouseButtons.Right);
         _middleMouse = new InputState<bool>(previous: _middleMouse.Current, current: (downMouseButtons & MouseButtons.Middle) == MouseButtons.Middle);
+    }
+
+    public void UpdateInputBuffer(IEnumerable<Input> buffer)
+    {
+        lock (_inputBuffer)
+        {
+            _inputBuffer.Clear();
+            _inputBuffer.AddRange(buffer);
+        }
     }
     
     internal bool IsLeftPressed() => IsPressed(_leftMouse);
@@ -76,12 +199,35 @@ public sealed class UIController
     internal bool IsHovering(string id) => _interactionStates.TryGetValue(id, out InteractionState interactionState) && interactionState.Hovering.Current;
     internal bool IsEntering(string id) => _interactionStates.TryGetValue(id, out InteractionState interactionState) && IsPressed(interactionState.Hovering);
     internal bool IsExiting(string id) => _interactionStates.TryGetValue(id, out InteractionState interactionState) && IsReleased(interactionState.Hovering);
-
+    
+    internal bool IsFocused(string id) => _lastInteractedID == id;
+    
+    internal bool IsPressed(Input input)
+    {
+        lock (_inputBuffer)
+        {
+            return _inputBuffer.Contains(input);
+        }
+    }
+    
+    internal IReadOnlyCollection<Input> GetInputBuffer()
+    {
+        lock (_inputBuffer)
+        {
+            return _inputBuffer;
+        }
+    }
+    
     internal void UpdateInteraction(string id, IntRect rect)
     {
         Position cursorPos = _cursor.Current;
         bool hovered = rect.Contains(cursorPos.X, cursorPos.Y);
         bool interacting = hovered && IsPressed(_leftMouse);
+
+        if (interacting)
+        {
+            _lastInteractedID = id;
+        }
 
         if (_interactionStates.TryGetValue(id, out InteractionState interactionState))
         {
