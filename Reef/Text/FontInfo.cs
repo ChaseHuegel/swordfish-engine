d32cc1438dbfcdefdabfdea7c1d0a0b9a67300a1
Seven <chase@chasehuegel.io>
2026-02-18 00:53:00 -0500

    Text layout line info and whitespace handling #461 #462

--- Reef/Text/FontInfo.cs
+++ Reef/Text/FontInfo.cs
@@ -6,6 +6,7 @@ public readonly struct FontInfo
     public readonly string Path;
     public readonly int MinUnicode;
     public readonly int MaxUnicode;
+    public readonly int TabSize;
 
     public FontInfo(string id, string path)
     {
@@ -13,13 +14,15 @@ public readonly struct FontInfo
         Path = path;
         MinUnicode = 0x0000;
         MaxUnicode = 0xffff;
+        TabSize = 4;
     }
     
-    public FontInfo(string id, string path, int minUnicode, int maxUnicode)
+    public FontInfo(string id, string path, int minUnicode, int maxUnicode, int tabSize)
     {
         ID = id;
         Path = path;
         MinUnicode = minUnicode;
         MaxUnicode = maxUnicode;
+        TabSize = tabSize;
     }
 }
