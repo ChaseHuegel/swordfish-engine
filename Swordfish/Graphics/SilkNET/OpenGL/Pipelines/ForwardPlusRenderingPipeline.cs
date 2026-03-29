ad2cfcbc6628cfe479eb1f0c2fca31121baafe8a
Seven <chase@chasehuegel.io>
2026-03-28 23:36:18 -0400

    Add bloom passes #490

--- Swordfish/Graphics/SilkNET/OpenGL/Pipelines/ForwardPlusRenderingPipeline.cs
+++ Swordfish/Graphics/SilkNET/OpenGL/Pipelines/ForwardPlusRenderingPipeline.cs
@@ -470,7 +470,7 @@ internal sealed unsafe class ForwardPlusRenderingPipeline<TRenderStage> : Render
         _renderFBO.Blit(ReadBufferMode.ColorAttachment0, DrawBufferMode.Back, ClearBufferMask.ColorBufferBit, BlitFramebufferFilter.Linear);
         _glDebug.TryLogError();
 
-        if (_renderSettings.Bloom.Get())
+        if (_renderSettings.Bloom.Enabled.Get())
         {
             //  Bloom pre-pass
             //  Blit the bloom renderbuffer to the blur texture
@@ -490,7 +490,7 @@ internal sealed unsafe class ForwardPlusRenderingPipeline<TRenderStage> : Render
                 _blurShader.SetUniform("texture0", 0);
 
                 var horizontal = true;
-                for (var i = 0; i <= 10; i++)
+                for (var i = 0; i <= _renderSettings.Bloom.Passes.Get(); i++)
                 {
                     int horizontalInt = horizontal ? 0 : 1;
                     horizontal = !horizontal;
